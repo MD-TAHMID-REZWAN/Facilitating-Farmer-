d5c232b02417dccba72b17bc74455ab9308bf87a
MD.TAHMID REZWAN <106175513+MD-TAHMID-REZWAN@users.noreply.github.com>
2023-08-08 21:30:05 +0600

    first commit

---
+++ BLL/Services/CustomerAuthServices.cs
@@ -0,0 +1,64 @@
+﻿using AutoMapper;
+using BLL.DTOs;
+using DAL;
+using DAL.EF.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Services
+{
+    public class CustomerAuthServices
+    {
+        static CustomerAuthServices()
+        {
+            var config = new MapperConfiguration(cfg => {
+                cfg.CreateMap<CustomerDTO, Customer>();
+                cfg.CreateMap<Customer, CustomerDTO>();
+            });
+            var mapper = new Mapper(config);
+        }
+
+        public static CustomerTokenDTO Authenticate(CustomerDTO user)
+        {
+            
+
+            var config = new MapperConfiguration(cfg => {
+                cfg.CreateMap<CustomerDTO, Customer>();
+                cfg.CreateMap<Customer, CustomerDTO>();
+                cfg.CreateMap<CustomerTokenDTO, CustomerToken>();
+                cfg.CreateMap<CustomerToken, CustomerTokenDTO>();
+            }
+            );
+
+            var mapper = new Mapper(config);
+            
+
+            
+            var data = mapper.Map<Customer>(user);
+
+            var result= DataAccessFactory.CustomerAuthDataAccess().Authenticate(data);
+            var token= mapper.Map<CustomerTokenDTO>(result);
+            return token;
+        }
+
+        public static bool IsAuthenticated(string token)
+        {
+            var rs = DataAccessFactory.CustomerAuthDataAccess().IsAuthenticated(token);
+            return rs;
+        }
+
+        public static bool TokenValidity(string token)
+        {
+            var tk = DataAccessFactory.CustomerTokensDataAccess().Get(token);
+            if (tk != null && tk.ExpiredAt == null)
+            {
+                return true;
+            }
+            return false;
+
+        }
+    }
+}
